1711f494cb7ef01e2b2dec9e9ac9e88ec1ffb07c
Pat Buxton <patrick.buxton@tartansolutions.com>
2025-10-02 11:13:40 +0100

    Align with starrocks

--- test/conftest.py
+++ test/conftest.py
@@ -16,8 +16,8 @@
 import pytest
 from sqlalchemy.dialects import registry
 
-registry.register("starrocks+pymysql", "sqlalchemy_starrocks.dialect", "StarRocksDialect")
-registry.register("starrocks", "sqlalchemy_starrocks.dialect", "StarRocksDialect")
+registry.register("starrocks+pymysql", "starrocks.dialect", "StarRocksDialect")
+registry.register("starrocks", "starrocks.dialect", "StarRocksDialect")
 pytest.register_assert_rewrite("sqlalchemy.testing.assertions")
 
 from sqlalchemy.testing.plugin.pytestplugin import *
