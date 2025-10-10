f88f7cef83f47fd6f951a09de79a545f30e0b157
Pat Buxton <patrick.buxton@tartansolutions.com>
2025-10-10 10:18:40 +0100

    Ensure copyright header and remove unecessary shebangs

--- starrocks/provision.py
+++ starrocks/provision.py
@@ -1,11 +1,26 @@
+# Copyright 2021-present StarRocks, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from sqlalchemy import exc
+
 from sqlalchemy.testing.provision import configure_follower
 from sqlalchemy.testing.provision import create_db
 from sqlalchemy.testing.provision import drop_db
-from sqlalchemy.testing.provision import generate_driver_url
 from sqlalchemy.testing.provision import temp_table_keyword_args
-from sqlalchemy.testing.provision import update_db_opts
+
+# from sqlalchemy import exc
+# from sqlalchemy.testing.provision import generate_driver_url
+# from sqlalchemy.testing.provision import update_db_opts
 #
 #
 # @generate_driver_url.for_db("starrocks")
